b84585697a65f93f2aea83ad38a4e4b014d8dd10
mac <mac@macs-MacBook-Air.local>
2023-07-26 14:14:08 +0300

    first commit

--- cypress/e2e/selectors.js
+++ cypress/e2e/selectors.js
@@ -1,15 +1,15 @@
 class LogInPage  {
     logInButton(){
-      return cy.get('#login-button')
+      return cy.get('#login-button');
     }
     logInError(){
-        return cy.get('.error-button')
+        return cy.get('.error-button');
     }
     userInputField(){
-        return cy.get('[placeholder="Username"]')    
+        return cy.get('[placeholder="Username"]');    
     }
     passwordInputField(){
-        return cy.get('[placeholder="Password"]')
+        return cy.get('[placeholder="Password"]');
     }   
     burgerMenuButton() {
       return cy.get("#react-burger-menu-btn");
